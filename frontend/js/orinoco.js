410a895b224cd448806b799b963392ab4007e0ee
EL WALID EL KHABOU <elkhabou@yahoo.fr>
2021-09-22 11:15:40 +0200

    Explication de depart dans orinoco.js

--- frontend/js/orinoco.js
+++ frontend/js/orinoco.js
@@ -1,6 +1,9 @@
 //-------------------------------------------------------------Récupération de la liste camera----------------------------------------------------//
 
-// Fonction permettant de récupérer la liste de produit du serveur
+// Fonction permettant de récupérer la liste de produit du serveur :
+// async fetch est une fonction asynchrone, 
+// await fetch('http....') lance une requête HTTP vers l'URL. Étant donné que le await est présent, 
+// la fonction asynchrone est suspendue jusqu'à ce que la demande se termine.
 async function loadCameras() {
     const response = await fetch('http://localhost:3000/api/cameras');
     if (!response.ok) {
@@ -9,6 +12,8 @@ async function loadCameras() {
     return await response.json();
 };
 
+// Lorsque la demande est terminée, la response est affecté à l'objet de réponse de la demande. 
+// et nous pouvons alors extraire des données utiles, comme JSON ou du texte brut, à partir de la réponse.
 // Fonction exécuter au chargement de la page
 async function onPageLoadCameras() {
     try {
